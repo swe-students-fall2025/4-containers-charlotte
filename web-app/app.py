5b166d3328ea892041d0b08d56dca1efcb2c8b85
SamuelYang242 <samuelyang0404@gmail.com>
2025-11-17 18:19:54 -0500

    Added db.py, and moved from flaskpymongo to pymongo

--- web-app/app.py
+++ web-app/app.py
@@ -4,29 +4,26 @@ import os
 import pathlib
 from typing import Optional
 
+import models
 from bson.objectid import ObjectId
+from db import db
 from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
-from flask_pymongo import PyMongo
-
-import models
 
 DIR = pathlib.Path(__file__).parent
 
 load_dotenv(DIR / ".env", override=True)
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 
-mongo = PyMongo(app)
 login_manager = LoginManager(app)
 
 
 @login_manager.user_loader
 def load_user(user_id: str) -> Optional[models.User]:
     """Load currently logged in user data"""
-    user_data = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    user_data = db.users.find_one({"_id": ObjectId(user_id)})
     if not user_data:
         return None
     return models.User(user_data)
--- web-app/auth.py
+++ web-app/auth.py
@@ -1,10 +1,10 @@
 """Authorization module for the web app"""
 
+import models
+from app import app
+from db import db
 from flask import flash, request
 from flask_login import current_user, login_required, login_user, logout_user
-
-import models
-from app import app, mongo
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -20,7 +20,7 @@ def login():
         if not username or not password:
             flash("Please provide both a username and password", "error")
 
-        user = mongo.db.users.find_one({"username": username})
+        user = db.users.find_one({"username": username})
         if user and user.check_password(password):
             login_user(user)
             flash("Logged in successfully!", "success")
@@ -39,14 +39,14 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        existing_user = mongo.db.users.find_one({"username": username})
+        existing_user = db.users.find_one({"username": username})
         if existing_user:
             flash("Username already exists. Please choose a different one.", "warning")
         else:
             user = models.User({"username": username})
             user.set_password(password)
-            inserted_id = mongo.db.users.insert_one(user)
-            login_user(mongo.db.users.find_one({"_id": inserted_id}))
+            inserted_id = db.users.insert_one(user)
+            login_user(db.users.find_one({"_id": inserted_id}))
             flash("Logged in successfully!", "success")
             return "User logged in" + user
     return "Reigstration page"
