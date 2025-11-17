c1a7207055010b9e96120d0b7e4cee5f7c820a72
SamuelYang24 <sy3638@nyu.edu>
2025-11-17 15:06:18 -0500

    Added template logic for login

---
+++ web-app/auth.py
@@ -0,0 +1,61 @@
+import models
+from app import app, mongo
+from flask import flash, request
+from flask_login import current_user, login_required, login_user, logout_user
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Login route"""
+
+    if current_user.is_authenticated:
+        return "User is already authenticated"
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if not username or not password:
+            flash("Please provide both a username and password", "error")
+
+        user = mongo.db.users.find_one({"username": username})
+        if user and user.check_password(password):
+            login_user(user)
+            flash("Logged in successfully!", "success")
+            return "User logged in" + user
+        else:
+            flash(
+                "Login Unsuccessful. Please check username and password",
+                "danger"
+            )
+    return "Login Page"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        "User is already authenticated"
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        existing_user = mongo.db.users.find_one({"username": username})
+        if existing_user:
+            flash(
+                "Username already exists. Please choose a different one.",
+                "warning"
+            )
+        else:
+            user = models.User(username=username)
+            user.set_password(password)
+            inserted_id = mongo.db.users.insert_one(user)
+            login_user(mongo.db.users.find_one({"_id": inserted_id}))
+            flash("Logged in successfully!", "success")
+            return "User logged in" + user
+    return "Reigstration page"
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "info")
+    return "User has been logged out"
