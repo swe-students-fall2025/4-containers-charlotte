26ca35d5d5c8bb3e74ca709071030df0b1470b92
hyunkyuu <kgankyu03@gmail.com>
2025-11-17 23:31:05 -0500

    fix: refine ML models and add docstrings

---
+++ machine-learning-client/app/models/__init__.py
@@ -0,0 +1,8 @@
+'''
+Machine learning models package
+'''
+
+from app.models.transcriber import Transcriber
+from app.models.voice_cloner import VoiceCloner
+
+__all__ = ['Transcriber', 'VoiceCloner']
