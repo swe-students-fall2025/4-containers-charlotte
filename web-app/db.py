72699c3901083b2bca37f88475e786f061785267
SamuelYang24 <sy3638@nyu.edu>
2025-11-18 12:53:48 -0500

    Re-added dotenv import

--- web-app/db.py
+++ web-app/db.py
@@ -1,8 +1,14 @@
 """Database operations for the web app"""
 
 import os
+import pathlib
 
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+DIR = pathlib.Path(__file__).parent
+
+load_dotenv(DIR / ".env", override=True)
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client.get_database(os.getenv("MONGO_DB"))
